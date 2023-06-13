913e971d7ed947a7865ed0e1d0f286c4a79ed89c
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-06-13 16:54:48 -0300

    fix: fix functions getUser and getUsers of teste1 of the controller

--- Controller/teste1.js
+++ Controller/teste1.js
@@ -1,17 +1,17 @@
-const data = require('../fakeData');
+const { teste1 } = require('../Service');
 
-const getUser = (req, res) => {
+const getUser = (req, res, next) => {
   const { name } = req.query;
-  for (let i = 0; i < data.length; i += 1) {
-    if (data[i].name === name) {
-      res.send(data[i]);
-    }
+  const user = teste1.getUser(name);
+
+  if (user.type === 'NOT FOUND') {
+    return next();
   }
+
+  return res.status(200).json(user);
 };
 
-const getUsers = (_req, res) => {
-  res.send(data);
-};
+const getUsers = (_req, res) => res.status(200).json(teste1.getUsers().response);
 
 module.exports = {
   getUser,
