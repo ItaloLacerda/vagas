207366f7c58c281a102372b09340366a2b497066
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-06-14 14:18:45 -0300

    feat: implements get method for route "/user/access"

--- Routes/user.router.js
+++ Routes/user.router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const {
-  teste1, teste2, teste3, teste4,
+  teste1, teste2, teste3, teste4, teste5,
 } = require('../Controller');
 
 const router = express.Router();
@@ -10,6 +10,6 @@ router.get('/', teste1.getUser, teste1.getUsers);
 router.post('/', teste2.postUser);
 router.delete('/', teste3.deleteUser);
 router.put('/', teste4.updateUser);
-// router.get("/users/access", teste5);
+router.get('/access', teste5.getCount);
 
 module.exports = router;
