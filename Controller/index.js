e1d92d0eb1a35e39fb580800e09b0fbadc60892a
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-06-14 12:11:18 -0300

    feat: update barrel of service and controller layers

--- Controller/index.js
+++ Controller/index.js
@@ -1,9 +1,13 @@
 const teste1 = require('./teste1');
 const teste2 = require('./teste2');
 const teste3 = require('./teste3');
+const teste4 = require('./teste4');
+// const teste5 = require('./teste5');
 
 module.exports = {
   teste1,
   teste2,
   teste3,
+  teste4,
+  // teste5,
 };
