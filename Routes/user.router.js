94574c70424f8cd3c5370e49b958e1beedc1c87a
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-06-14 09:40:20 -0300

    feat: implements post method for "/user" route

--- Routes/user.router.js
+++ Routes/user.router.js
@@ -1,14 +1,11 @@
 const express = require('express');
 
-const { teste1 } = require('../Controller');
+const { teste1, teste2 } = require('../Controller');
 
 const router = express.Router();
 
 router.get('/', teste1.getUser, teste1.getUsers);
-
-// router.get('/', );
-
-// router.post("/users", teste2)
+router.post('/', teste2.postUser);
 // router.delete("/users", teste3)
 // router.put("/users", teste4)
 // router.get("/users/access", teste5);
