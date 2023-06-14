7e51b40de69675a8002449612dcc64949b0f821a
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-06-14 09:32:44 -0300

    fix: implements name validation in the "getUser" method in the service layer

--- Service/teste1.js
+++ Service/teste1.js
@@ -1,10 +1,11 @@
 const { fakeDataModel } = require('../database/model');
 
 const getUser = (name) => {
-  const user = fakeDataModel.findOne(name);
-
-  if (user.length === 1) {
-    return { type: '', response: user[0] };
+  if (name) {
+    const user = fakeDataModel.findOne(name);
+    if (user.length === 1) {
+      return { type: '', response: user[0] };
+    }
   }
 
   return { type: 'NOT FOUND', response: 'User not found' };
