7c10b6225d3c9615f08657735950e942311c2c17
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-06-14 12:11:53 -0300

    feat: create put method for route "/user"

--- Routes/user.router.js
+++ Routes/user.router.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
-const { teste1, teste2, teste3 } = require('../Controller');
+const {
+  teste1, teste2, teste3, teste4,
+} = require('../Controller');
 
 const router = express.Router();
 
 router.get('/', teste1.getUser, teste1.getUsers);
 router.post('/', teste2.postUser);
 router.delete('/', teste3.deleteUser);
-// router.put("/users", teste4)
+router.put('/', teste4.updateUser);
 // router.get("/users/access", teste5);
 
 module.exports = router;
