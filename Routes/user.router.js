b3a0e503d84d670aedf6b388728cd754eebf3499
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-06-14 10:33:51 -0300

    feat: create delete method for route "user"

--- Routes/user.router.js
+++ Routes/user.router.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-const { teste1, teste2 } = require('../Controller');
+const { teste1, teste2, teste3 } = require('../Controller');
 
 const router = express.Router();
 
 router.get('/', teste1.getUser, teste1.getUsers);
 router.post('/', teste2.postUser);
-// router.delete("/users", teste3)
+router.delete('/', teste3.deleteUser);
 // router.put("/users", teste4)
 // router.get("/users/access", teste5);
 
