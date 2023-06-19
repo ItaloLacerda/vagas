261101c42b07fa07cd9712f8bc15b520236da411
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-06-19 09:33:24 -0300

    feat: implement middlewares in "/user" routes

--- Routes/user.router.js
+++ Routes/user.router.js
@@ -1,15 +1,16 @@
 const express = require('express');
 
+const { isUser, isAdmin } = require('../middlewares');
 const {
   teste1, teste2, teste3, teste4, teste5,
 } = require('../Controller');
 
 const router = express.Router();
 
-router.get('/', teste1.getUser, teste1.getUsers);
-router.post('/', teste2.postUser);
-router.delete('/', teste3.deleteUser);
-router.put('/', teste4.updateUser);
+router.get('/', isUser, teste1.getUser, teste1.getUsers);
+router.post('/', isUser, teste2.postUser);
+router.delete('/', isUser, isAdmin, teste3.deleteUser);
+router.put('/', isUser, isAdmin, teste4.updateUser);
 router.get('/access', teste5.getCount);
 
 module.exports = router;
