cd6376b0d5a7a7cc0b3776b355e6852da7d9a05a
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-06-19 09:26:54 -0300

    feat: update fakeData

--- database/fakeData.js
+++ database/fakeData.js
@@ -3,6 +3,8 @@ const fakeData = [
     id: 1,
     name: 'João Oliveira',
     job: 'Desenvolvedor',
+    role: 'Admin',
+    password: '$2b$10$wJ9e4HbvT4pBE4X2UR2gquku5FoJJlGbHUzDfzfVQwf7GHyByfSI6', // senha: 'minhaSenhaSuperSegura'
   },
 ];
 
